12bb6d46cf07682218730c9c71df100674bd4037
danyal lorestany <danyallsty04@gmail.com>
2026-02-16 16:04:45 +0330

    ts config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import StationsProvider from "./context/StationProvider.tsx";
+import App from "./App";
+import StationsProvider from "./context/StationProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StationsProvider>
